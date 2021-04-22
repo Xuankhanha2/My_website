ee94572f812609cfe0b6b2e388209b70c26557ef
VU_XUAN_KHANH <VU_XUAN_KHANH@DESKTOP-E0G8LG9>
2021-04-22 19:40:15 +0700

    update BaseClass and format money

---
+++ Models/BaseClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrangChuWebsite.Models
+{
+    public class BaseClass
+    {
+        public string formatMoney(double money)
+        {
+            string newMoney = "";
+            newMoney = String.Format("{0:0,0}", money);
+            return newMoney;
+        }
+    }
+}
